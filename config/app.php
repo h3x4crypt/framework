cc0845ee27fad64160bf70fa6a72b1a59b7cbbb1
h3x4crypt <theworldeye1@gmail.com>
2018-07-30 17:29:56 +0300

    major-fix0x06

--- config/app.php
+++ config/app.php
@@ -13,8 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
-
+    'name' => env('APP_NAME', 'h3x4'),
     /*
     |--------------------------------------------------------------------------
     | Application Environment
@@ -39,7 +38,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +51,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://master.iechosys.com:813/'),
 
     /*
     |--------------------------------------------------------------------------
